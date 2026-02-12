8cc5856070261d21f4eddf7d429b7fe59c9b965b
kas1104 <criatura052@gmail.com>
2026-02-12 10:11:07 -0300

    Alterações feitas

--- src/aulaOrientacao.java
+++ src/aulaOrientacao.java
@@ -1,19 +1,36 @@
+//Importei a classe entidade
 import professor.entity.Professor;
+//biblioteca Scanner para receber dados do usuario
 import java.util.Scanner;
 
 public class aulaOrientacao {
+        //Função principal que vai execcutar o codigo
     public static void main(String[] args){
+        //Objeto sc que vai instaciar a classe Scanner e que vai puxar os dados atribuidos pelo usuario
         Scanner sc = new Scanner(System.in);
-        Professor professor1 = new Professor(" ");
+
+        System.out.println("Digite o tamanho:");
+        int tamanhousuario = sc.nextInt();
 
 
-        professor1.addProfessor("joao");
-        professor1.addProfessor("Carlos");
-        professor1.addProfessor("Felipe");
+        //Segundo objeto instaciado chamado professor2 em que ele recebe o tamanho do usuario e vai atribuir o tamanho do usuario ao atributo tamanho
+        Professor professor2 = new Professor(tamanhousuario);
 
+        //Parametro que limpa a memoria para não ocorrer nenhum erro!
+        sc.nextLine();
 
-        for(int x = 0; x < professor1.getNomesprofessores().length; x++){
-            System.out.println(professor1.getNomesprofessores()[x]);
+        //Estrutura de repetição que atribui um nome a uma posição dentro da lista professor e o tamanho é definido pelo metodo getTamanho da classe entidade
+        for(int x = 0; x < professor2.getTamanho(); x++){
+            System.out.println("Digite o nome:");
+            String nome = sc.nextLine();
+
+            //Objeto que chama o metodo adicionarProfessor em que vai receber o nome de cada professor e atribuir a uma posição
+            professor2.addProfessor(nome);
+        }
+
+        //Estrutura de repetição que vai receber um objeto que puxa o metodo getNomeProfessor que vai pegar o nome dos professores da lista e exibir cada um em sua posição
+        for(int x = 0; x < professor2.getNomesprofessores().length; x++){
+            System.out.println(professor2.getNomesprofessores()[x]);
         }
     }
 }
--- src/professor/entity/Professor.java
+++ src/professor/entity/Professor.java
@@ -1,30 +1,27 @@
 package professor.entity;
 
 public class Professor {
+    //Atributos da classe entidade Professor
     private String[] nomesprofessores;
-    private String nome;
     private static int contador;
     private int tamanho;
 
-
-    public Professor(String nome){
-        this.nome = nome;
-        nomesprofessores = new String[10];
+    //Construtor que possui a variavel int tamanho e preciso de um construtor par aatribuir o tamanho da lista
+    public Professor(int tamanho){
+        this.tamanho = tamanho;
+        nomesprofessores = new String[tamanho];
     }
 
-
+    //Metodo que é utilizado para retorna os nomes dentro da lista Nomesprofessores ao usuario
     public String[] getNomesprofessores() {
         return nomesprofessores;
     }
 
-    public void setTamanho(int tamanho) {
-        this.tamanho = tamanho;
-    }
-
+    //Metodo que pega o atributo tamanho definido pelo usuario
     public int getTamanho() {
         return tamanho;
     }
-
+        //Metodo criado para adicionar nomes a lista nomesprofessores e que possui um contador estatico que a cada posição adiciona um valor a lista
     public void addProfessor(String nomeDoproprioMetodo){
         nomesprofessores[contador] = nomeDoproprioMetodo;
         contador++;
